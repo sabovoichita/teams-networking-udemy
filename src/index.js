acfa4b0df56b279328ff88484fdc0644a16721fd
Voichita Maria <sabo_voichita@yahoo.com>
2024-02-02 13:12:31 +0100

    filter elements

--- src/index.js
+++ src/index.js
@@ -126,7 +126,26 @@ function getTeamValues() {
   };
 }
 
+function filterElements(search) {
+  search = search.toLowerCase();
+  //   console.warn("search %o", search);
+  return allTeams.filter(team => {
+    return (
+      team.promotion.toLowerCase().includes(search) ||
+      team.members.toLowerCase().includes(search) ||
+      team.name.toLowerCase().includes(search) ||
+      team.url.toLowerCase().includes(search)
+    );
+  });
+}
+
 function initEvents() {
+  $("#search").addEventListener("input", e => {
+    const search = e.target.value;
+    const teams = filterElements(search);
+    renderTeams(teams);
+  });
+
   $("#teamForm").addEventListener("submit", onSubmit);
   $("#teamForm").addEventListener("reset", () => {
     console.warn("reset", editId);
