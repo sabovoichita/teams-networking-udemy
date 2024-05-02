a70e553512efb151a781324c3424e32683b31fcb
Voichita Maria <sabo_voichita@yahoo.com>
2024-05-02 10:41:14 +0200

    add data folder and demo settings

--- src/middleware.js
+++ src/middleware.js
@@ -1,4 +1,37 @@
-const baseUrl = "http://localhost:3000/teams-json";
+// for demo purposes...
+const isDemo = location.host === "sabovoichita.github.io";
+//const inlineChanges = isDemo;
+// const baseUrl = "http://localhost:3000/teams-json";
+
+const API = {
+  CREATE: {
+    URL: "http://localhost:3000/teams-json/create",
+    METHOD: "POST"
+  },
+  READ: {
+    URL: "http://localhost:3000/teams-json",
+    METHOD: "GET"
+  },
+  UPDATE: {
+    URL: "http://localhost:3000/teams-json/update",
+    METHOD: "PUT"
+  },
+  DELETE: {
+    URL: "http://localhost:3000/teams-json/delete",
+    METHOD: "DELETE"
+  }
+};
+
+if (isDemo) {
+  API.READ.URL = "./data/teams.json";
+  API.DELETE.URL = "./data/delete.json";
+  API.CREATE.URL = "./data/create.json";
+  API.UPDATE.URL = "./data/update.json";
+
+  API.DELETE.METHOD = "GET";
+  API.CREATE.METHOD = "GET";
+  API.UPDATE.METHOD = "GET";
+}
 
 export function loadTeamsRequest() {
   return fetch(baseUrl, {
